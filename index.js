c7abac355646fba8c7123d54fbcf5e18aaa81eb1
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-04 11:52:01 +0700

    update solution

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const alphabets = [
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z'
+];
+
 function createNestedArr(jumlahRow, jumlahCol) {
     let result = [];
     for(let i = 0; i < jumlahRow; i++) {
         result[i] = [];
         for(let j = 0; j < jumlahCol; j++) {
-            result[i][j] = String.fromCharCode(Math.floor(Math.random() * Math.floor(26) + 97));
+            let randomIndex = Math.floor(Math.random() * Math.floor(25));
+            result[i][j] = alphabets[randomIndex];
         }
     }
     
