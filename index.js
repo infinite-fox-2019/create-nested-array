b899c10b01df7566ca940d9a427162647b0f2219
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-04 10:36:57 +0700

    fixing indentation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function createNestedArr(jumlahRow, jumlahCol) {
 let alphabets = 'abcdefghijklmnopqrstuvwxyz'
 let res = []
-for (let i=0; i<jumlahRow; i++) {
-    let rows = []
-    for (let j=0; j<jumlahCol; j++) {
-        rows.push(alphabets.charAt(Math.floor(Math.random() * alphabets.length)))
+    for (let i=0; i<jumlahRow; i++) {
+        let rows = []
+            for (let j=0; j<jumlahCol; j++) {
+                rows.push(alphabets.charAt(Math.floor(Math.random() * alphabets.length)))
+        }
+            res.push(rows)
     }
-    res.push(rows)
-}
 return res
 }
 console.log(createNestedArr(5, 3));
